b0bd15d182326aad76f69bd7e91a20c745e593ff
Solon <Solonerus@gmail.com>
2023-05-25 00:06:59 +0100

    Adds code to worker to handle making new rooms

--- src/creeps/Worker.ts
+++ src/creeps/Worker.ts
@@ -15,6 +15,60 @@ export class Worker {
         }
       }
     }
+
+    // If this creep has a state of 'spawnMaker' then it should go to the 'claim' flag and being working on the spawn
+    if (creep.memory.state == "spawnMaker") {
+      // Find the claim flag
+      const flag = Game.flags["claim"];
+
+      if (!flag || creep.room.name != flag.room?.name) {
+        // Move to it
+        creep.moveTo(flag, { visualizePathStyle: { stroke: "#ffffff" }, reusePath: 5 });
+      } else {
+        // If a spawner exists in the room, delete the claim flag
+        const spawner = creep.room.find(FIND_MY_SPAWNS)[0];
+        if (spawner) {
+          creep.suicide()
+        }
+
+        // If the creep's memory doesn't have a 'building' property, add it with a value of false
+        if (!creep.memory.building) {
+          creep.memory.building = false;
+        }
+
+        // If the creep's store is empty, set the building state to false
+        if (creep.store.getUsedCapacity() == 0) {
+          creep.memory.building = false;
+        }
+
+        // If the creep's store is full, set the building state to true
+        if (creep.store.getUsedCapacity() == creep.store.getCapacity()) {
+          creep.memory.building = true;
+        }
+
+        // If the creep's building state is false, mine energy
+        if (!creep.memory.building) {
+          // Find the closest source
+          const source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+          if (source) {
+            // Harvest the source
+            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+              // Move to it
+              creep.moveTo(source, { visualizePathStyle: { stroke: "#ffaa00" }, reusePath: 5 });
+            }
+          }
+        }
+        // If the creep's building state is true, build the spawn
+        else {
+          const target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+          if (target && creep.build(target) == ERR_NOT_IN_RANGE) {
+            // Move into range
+            creep.moveTo(target, { visualizePathStyle: { stroke: "#ffffff" }, reusePath: 5 });
+          }
+        }
+      }
+    }
+
     // If it does not, resume normal operation
     else {
       if (this.retrieveEnergy(creep, false)) {
